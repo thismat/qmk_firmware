aff2ea17eab7f6ec6be0a66aed46334b2cafcdf3
thismat <m@thismat.com>
2021-02-03 14:45:13 -0600

    Added layer lighting, mod tap keys, layer tap keys

--- keyboards/kbdfans/kbd75/keymaps/thismat/config.h
+++ keyboards/kbdfans/kbd75/keymaps/thismat/config.h
@@ -1,3 +1,14 @@
+#pragma once
+
+
+// Change how the device is displayed
+#undef MANUFACTURER
+#undef PRODUCT
+#define MANUFACTURER    MATT
+#define PRODUCT         Gabby
+
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
+
+#define RGBLIGHT_LAYERS
